db8f1b81577b02dad4a6a366b52952e06111b7e0
Robbie Jaeger <jaeger.rob@gmail.com>
2022-01-13 13:17:38 -0700

    Edit site coordinates to be more accurate to the map

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,8 @@ const options: cors.CorsOptions = {
 app.use(cors(options));
 
 const campsites:Campsite[] = [
-  {title: "Big Pool", location: {lat: 40.2744767, long: -105.7643305}, datesAvailable: [], elevation: "9,160 ft", detailsLink: "https://www.nps.gov/romo/planyourvisit/upload/072-Big-Pool-2017-2.pdf"},
-  {title: "North Inlet Falls", location: {lat: 40.269919, long: -105.7248948}, datesAvailable: [], elevation: "9,540 ft", detailsLink: "https://www.nps.gov/romo/planyourvisit/upload/079-North-Inlet-Falls-2021-1.pdf"}
+  {title: "Big Pool", location: {lat: 40.285032, long: -105.759724}, datesAvailable: [], elevation: "9,160 ft", detailsLink: "https://www.nps.gov/romo/planyourvisit/upload/072-Big-Pool-2017-2.pdf"},
+  {title: "North Inlet Falls", location: {lat: 40.278834, long: -105.720641}, datesAvailable: [], elevation: "9,540 ft", detailsLink: "https://www.nps.gov/romo/planyourvisit/upload/079-North-Inlet-Falls-2021-1.pdf"}
 ];
 
 app.get('/api/v1/campsites', (_request, response) => {
