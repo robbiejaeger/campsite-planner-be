cb1186499eab2ecce3bdb34ad2d52526b0f404ee
Robbie Jaeger <jaeger.rob@gmail.com>
2022-01-15 07:06:55 -0700

    Remove campsites with duplicate names

--- src/server.ts
+++ src/server.ts
@@ -15,19 +15,31 @@ app.use(cors(options));
 // Parse campsites data file
 let campsites:Campsite[] = [];
 
+function isDuplicate(uniqueCampsites:Campsite[], rawSite:any) {
+  return uniqueCampsites.find(campsite => {
+    return campsite.title === rawSite.properties.Campsite;
+  });
+}
+
 readFile('./raw_data/Backcountry_Campsites_for_2017.geojson', {encoding: 'utf-8'})
   .then(fileContents => {
     const rawSites = JSON.parse(fileContents).features as any[];
-    campsites = rawSites.map(rawSite => {
-      const siteProperties = rawSite.properties;
-      const groupSiteBoolean = siteProperties.GroupSite === "Yes" ? true : false;
-      return {
-        title: siteProperties.Campsite,
-        groupSite: groupSiteBoolean,
-        location: {lat: siteProperties.WGS84_LAT_DD ,long: siteProperties.WGS84_LON_DD},
-        datesAvailable: []
-      };
-    })
+
+    // iterate over the raw campsite data
+    campsites = rawSites.reduce((uniqueCampsites, rawSite) => {
+      if (!isDuplicate(uniqueCampsites, rawSite)) {
+        const siteProperties = rawSite.properties;
+        const groupSiteBoolean = siteProperties.GroupSite === "Yes" ? true : false;
+
+        uniqueCampsites.push({
+          title: siteProperties.Campsite,
+          groupSite: groupSiteBoolean,
+          location: {lat: siteProperties.WGS84_LAT_DD ,long: siteProperties.WGS84_LON_DD},
+          datesAvailable: []
+        });
+      }
+      return uniqueCampsites;
+    }, []);
   })
   .catch(err => console.error('Error reading campsites data file:', err));
 
